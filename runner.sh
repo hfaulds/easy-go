eae3822242149f5bb303a0bc76f995acff52266e
Hayden Faulds <fauldsh@gmail.com>
2019-02-18 11:45:50 +0000

    debugging

--- runner.sh
+++ runner.sh
@@ -4,6 +4,12 @@ set -e
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
+
+if [ "$DOCKER_USERNAME" == '[FILTERED]' ]; then
+  echo "RUH ROH!"
+  exit 1
+fi
+
 image="$DOCKER_USERNAME/$ACTION_CACHE_NAME"
 
 if [ -n "$1" ]; then
